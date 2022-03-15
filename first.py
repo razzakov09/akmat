e5029aaf3ced38d3394cc81e717bbedfbe91a47c
razzakov09 <akmat.tashpolot@gmail.com>
2022-03-15 20:54:39 +0600

    first commit

---
+++ first.py
@@ -0,0 +1,98 @@
+'''problem 1
+num1 = 17925
+num2 = 34**2
+num3 = 26*3
+num4 = 17**33
+num5 = 4394*4
+
+print(num1 >= num2)
+print(num1 >= num3)
+print(num1 >= num4)
+print(num1 >= num5)
+
+#problem 2
+a = 10
+b = 5
+c = 15
+print(a > b)
+print(a < c)
+
+a1 = 7 % 3 * 4.8
+print(a1)
+
+a2 = 15 + 5
+b2 = 30 - 10
+print(a2 == b2)
+
+a3 = 35 / 7
+b3 = 45 / 5
+print(a3 != b3)
+'''
+'''
+
+#problem 3
+num1 = 34 + 75
+num2 = num1 * 7
+num3 = num2**2
+num4 = num3 - 193432
+print(num4)
+
+#problem 4
+a = 17*3
+b = 12*5
+print(a<b)
+c = 12**3
+d = 13*7
+print(c > d)
+i = 4**5
+f = 512+512
+print(i == f)
+'''
+'''
+#problem 5
+num1 = -21//10
+print(num1)
+
+#problem 6
+a = 16
+b = 100
+num2 = 16/100 * 100
+print(num2)
+
+#problem 7
+a2005 = 2005
+a2020 = 2020
+a2022 = 2022
+print(a2020 - a2005)
+print(a2022 - a2005)
+'''
+
+'''
+#problem 8
+num1 = 25
+num2 = 75
+num3 = 10
+num4 = 95
+num = num1 + num2 + num3 + num4
+num5 = num / 4
+print(num5)
+'''
+'''
+#problem 9 
+a = 4
+b = 4
+c = 4
+d = (a+b)/c
+print(d)
+''''
+#problem 10
+a = 4
+b = 14
+c = 4.9
+d = 15
+e = 1
+
+#problem 11
+f = (a - e**(b/d))%c
+print(f)
+# akmat
