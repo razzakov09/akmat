eeafced7cda6c447f1811c675d0dc04ad0ae9045
razzakov09 <akmat.tashpolot@gmail.com>
2022-03-15 21:01:39 +0600

    if_elif_else

---
+++ akmat1103.py
@@ -0,0 +1,130 @@
+'''
+#problem 1
+num1 = 2**3
+num2 = 3**2
+if num1>num2:
+    print(num1)
+else :
+    print(num2)
+'''
+
+'''
+#problem 2
+num1 = int(input('Введите число: '))
+num2 = int(input('Введите число: '))
+num3 = int(input('Введите число: '))
+if num1>num2 and num1>num3:
+    print(num1)
+elif num2>num1 and num2>num3:
+    print(num2)
+else :
+    print(num3)
+'''
+
+'''
+#problem 3
+num1 = 17391 % 17
+num2 = 546 % 17
+num3 = 934 % 17
+if num1 < num2 and num1<num3:
+    print(num1)
+elif num2 < num1 and num2<num3:
+    print(num2)
+else :
+    print(num3)
+'''
+
+'''
+#problem 4
+x = 13
+x2 = 13**2
+if x2>172:
+    print('Возведи в квадрат!')
+elif x2**2>172:
+    print('Задание выполнено!')
+else :
+    print('Ошибка!')
+'''
+
+'''
+#problem 5
+num1 = int(input('Введите число: '))
+if num1%2 == 0:
+    print("Число четное")
+else:
+    print("Число не четное")
+if  num1%3 == 0:
+    print('Число делиться без остатка на 3')
+else:
+    print('Число не делиться на 3 без остатка')
+if num1**2 < 1000:
+    print('1000 больше нашего числа')
+else:
+    print('Наше число больше 1000')
+'''
+
+'''
+#problem 6 
+x = int(input('Введите число: '))
+if x>=0 and x<=21:
+    print('OK')
+elif x>=57 and x<=100:
+    print('OK')
+else:
+    print('Error')
+'''
+
+'''
+#problem 7 
+x = int(input('Введите число: '))
+if x = x
+    ("OK")
+else :
+    ('Error')
+'''
+
+'''
+#problem 8
+x = int(input('Введите число: '))
+if x > 75:
+    print("Ваше число > 75")
+else:
+    if x > 50:
+        print('Ваше число > 50')
+    else:
+        if x> 25:
+            print('Ваше число > 25')
+        else:
+            print('Ваше число < 25')
+'''
+'''
+#problem 9 
+a = 10//5
+b = 10/5
+if a==b:
+    print('Они равны')
+    print(a + b)
+else :
+    print('Они не равны')
+'''
+'''
+#problem 10
+x =  int(input('Введите число: '))
+if x<0:
+    print(x)
+else :
+    print()
+'''
+
+#problem 11
+a = 10
+b = 5
+if a>=0 and b>=5:
+    print('Числа положительные')
+else:
+    print('Числа отрицательные')
+#problem 12
+if  a > b:
+    print(a + 2)
+else:
+    print(b + 2)
